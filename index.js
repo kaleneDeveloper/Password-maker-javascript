bd6301c2d83b445056fcd78c22d4eb7c589686c3
kalene APIOU <apiou.kalene@gmail.com>
2021-08-10 00:23:36 +0200

    update

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const displayPass = document.getElementById("password-output");
 const generatePassword = () => {
     let data = [];
     let password = "";
+    let x = 0;
+
     lowercase.checked ? data.push(...dataLowercase) : null;
     uppercase.checked ? data.push(...dataUppercase) : null;
     numbers.checked ? data.push(...dataNumbers) : null;
@@ -23,10 +25,47 @@ const generatePassword = () => {
         alert("Please select box");
         return;
     } else {
-        for (i = 0; i < rangeValue.value; i++) {
+        if (lowercase.checked) {
+            const splitDataLowercase = dataLowercase.split("");
+            for (i = 0; i < 1; i++) {
+                dataIndex = Math.floor(
+                    Math.random() * splitDataLowercase.length
+                );
+                password += splitDataLowercase[dataIndex];
+            }
+            x++;
+        }
+        if (uppercase.checked) {
+            const splitDatauppercase = dataUppercase.split("");
+            for (i = 0; i < 1; i++) {
+                dataIndex = Math.floor(
+                    Math.random() * splitDatauppercase.length
+                );
+                password += splitDatauppercase[dataIndex];
+            }
+            x++;
+        }
+        if (numbers.checked) {
+            const splitDatanumbers = dataNumbers.split("");
+            for (i = 0; i < 1; i++) {
+                dataIndex = Math.floor(Math.random() * splitDatanumbers.length);
+                password += splitDatanumbers[dataIndex];
+            }
+            x++;
+        }
+        if (symbols.checked) {
+            const splitDatasymbols = dataSymbols.split("");
+            for (i = 0; i < 1; i++) {
+                dataIndex = Math.floor(Math.random() * splitDatasymbols.length);
+                password += splitDatasymbols[dataIndex];
+            }
+            x++;
+        }
+        for (i = 0; i < rangeValue.value - x; i++) {
             dataIndex = Math.floor(Math.random() * data.length);
             password += data[dataIndex];
         }
+
         displayPass.value = password;
         displayPass.select();
         document.execCommand("copy");
